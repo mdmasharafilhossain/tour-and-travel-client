8b329b31181218395fb777b8a14033c4731cb212
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2023-11-05 18:24:39 +0600

    basic react-router setup done

---
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import error from '../../assets/404-error-page-found_125755-1.jpg'
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <img src={error} alt="" />
+        </div>
+    );
+};
+
+export default ErrorPage;

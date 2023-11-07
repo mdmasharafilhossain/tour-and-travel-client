c3b35c076b0d7336d4ab9c63e023177ff42cd987
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2023-11-07 11:20:21 +0600

    Firebase install and basic setup done

---
+++ src/FireBase/FireBase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA0qX3CwGoibMlVC9sat-ERM9Htbzlw-J0",
+  authDomain: "travel-and-tour-86fbe.firebaseapp.com",
+  projectId: "travel-and-tour-86fbe",
+  storageBucket: "travel-and-tour-86fbe.appspot.com",
+  messagingSenderId: "741109151755",
+  appId: "1:741109151755:web:93b59236f9a9d999706f0e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;

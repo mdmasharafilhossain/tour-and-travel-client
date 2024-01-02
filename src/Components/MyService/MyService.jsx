82906af911bb8727a5f2b7875711187e8d0ba3c6
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2024-01-02 20:50:37 +0600

    my added service section added

--- src/Components/MyService/MyService.jsx
+++ src/Components/MyService/MyService.jsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../AuthProviders/AuthProviders";
 import axios from "axios";
+import MyServiceDetails from "./MyServiceDetails/MyServiceDetails";
 
 
 const MyService = () => {
@@ -24,8 +25,21 @@ const MyService = () => {
 
     console.log(Service)
     return (
-        <div className="mt-20">
-           <h1>length :{Service.length}</h1>
+        <div className="mt-20 mb-20">
+           <h1 className="text-3xl font-bold text-orange-600 mt-28 mb-20 text-center">Your Added Service :{Service.length}</h1>
+            
+            
+           {Service.length === 0 ? (
+                <p className="text-center text-red-600 mt-60 text-6xl font-semibold">
+                    No Services Available!!!!
+                </p>
+            ) : (
+                <div className="grid container mx-auto grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+                    {Service.map((mySer) => (
+                        <MyServiceDetails key={mySer._id} mySer={mySer} />
+                    ))}
+                </div>
+            )}
         </div>
     );
 };
